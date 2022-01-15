6962c6c8522f85e8f6e2bd95fbfda04294956af1
Tuan Dinh <tdinh@hamming.local>
2022-01-15 16:33:27 -0600

    submit to icml

--- download.sh
+++
@@ -1,2 +0,0 @@
-rsync -av  dthai_umass_edu@unity.rc.umass.edu:~/scratch/tprojects/repgan/src ./ --exclude=.pth
-
--- upload.sh
+++
@@ -1,2 +0,0 @@
-rsync -av ./src dthai@node100:/mnt/nfs/scratch1/dthai/Projects/repgan/ --exclude=.pyc
-
